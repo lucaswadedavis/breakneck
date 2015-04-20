20039a34ffca70d63bca7985cd4488b9f95e85ea
Luke Davis <lukefable@gmail.com>
2015-04-20 21:43:26 +0000

    adds a few notes, a test dict for phobos, and a deorbit function.

--- breackneck.py
+++ breackneck.py
@@ -1,5 +1,21 @@
 # simple 4 step calculation
 # step 1: how much energy (and as a consequence, how big a rocket) would be needed to push Phobos into Mars
+## I'm really interested in using astropy for this, but I actually think their use-case is a bit more thourough than I have need of - at least for the initial de-orbiting phase
+phobos = {
+    'mass' : 1.0659e16, #kg
+    'apoapsis' : 9517.58, #km
+    'periapsis' : 9234.42, #km
+    'orbital_speed' : 2.138 #km/s
+    }
+
+def deorbit (asteroid):
+    #takes an asteroid (phobos or deimos) and returns the power necessary to deorbit the thing
+    #what is the orbital speed necessary to keep phobos in orbit?
+    #constraint: orbit degredation should be precipitous - no more than 100 days
+    #just a test to make sure everything's working
+    print asteroid['mass']
+
+deorbit(phobos)
 
 # step 2: how much ejecta would be produced by a hit from the asteroid
 
