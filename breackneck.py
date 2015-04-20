21b366f3a5ae3207210f5dc3a3eb8f5a6432e913
Luke Davis <lukefable@gmail.com>
2015-04-20 20:58:03 +0000

    added the py file and a set of rough outlines

---
+++ breackneck.py
@@ -0,0 +1,9 @@
+# simple 4 step calculation
+# step 1: how much energy (and as a consequence, how big a rocket) would be needed to push Phobos into Mars
+
+# step 2: how much ejecta would be produced by a hit from the asteroid
+
+# step 3: how much would that ejecta falling back to the Martian surface heat the atmosphere
+
+# step 4: how much (and for how long) would the atmosphere need to be heated in order to sublimate a sufficient amount of the frozen CO2 and H2O in order to create a self-sustaining, thick, warm Martian atmosphere.
+
